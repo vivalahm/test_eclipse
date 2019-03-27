16b8e37264f8e4d0469ca393b553b5a3890a5a6b
HM <vivalahm@gmail.com>
2019-03-27 11:23:43 +0900

    merge fix

--- test_java_project/src/test_java_project/test_class.java
+++ test_java_project/src/test_java_project/test_class.java
@@ -4,10 +4,7 @@ public class test_class {
 
 	public static void main(String[]args) {
 		System.out.println("Hello World~! hotfix!");
-<<<<<<< HEAD
 		System.out.println("I'm the master~!!");
-=======
 		System.out.println("I'm the hotfix~!");
->>>>>>> refs/heads/hotfix
 	}
 }
